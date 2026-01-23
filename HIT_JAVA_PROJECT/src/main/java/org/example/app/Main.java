353230de64f33f71cf1c31133cc78deccca0e35c
maiHuong1311 <maihunw@gmail.com>
2026-01-23 18:12:15 +0700

    feat: add Main and AppLauncher to start the application

---
+++ HIT_JAVA_PROJECT/src/main/java/org/example/app/Main.java
@@ -0,0 +1,27 @@
+package org.example.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/register.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Hệ thống Đăng ký");
+
+        Scene scene = new Scene(root, 520, 480);
+
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
