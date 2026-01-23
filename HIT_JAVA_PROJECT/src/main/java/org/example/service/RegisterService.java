b00c86515a049edf51e8d2861c59a073ca599a9d
maiHuong1311 <maihunw@gmail.com>
2026-01-23 18:06:28 +0700

    refactor(RegisterService): simplify exception handling

--- HIT_JAVA_PROJECT/src/main/java/org/example/service/RegisterService.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/service/RegisterService.java
@@ -4,30 +4,24 @@ import org.example.constant.Common;
 import org.example.constant.ErrorMessage;
 import org.example.dao.UserDAO;
 import org.example.model.User;
+import org.example.utils.EmailUtil;
+import org.example.utils.PasswordUtil;
 
 import java.util.regex.Pattern;
 
 public class RegisterService {
     private UserDAO userDAO = new UserDAO();
-    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z]).{8,}$";
-    private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@(.+)$";
-    public boolean registerService(User user) {
-        if (user.getUsername() == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_USERNAME);
-        if (user.getPassword() == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_PASSWORD);
-        if (user.getFullName() == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_FULL_NAME);
-        if (user.getEmail() == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_EMAIL);
-        if(!Pattern.compile(EMAIL_PATTERN).matcher(user.getEmail()).matches())
-            throw new IllegalArgumentException(ErrorMessage.INVALID_EMAIL);
-        if(!Pattern.compile(PASSWORD_PATTERN).matcher(user.getPassword()).matches())
-            throw new IllegalArgumentException(ErrorMessage.INVALID_PASSWORD);
+    public boolean registerService(User user) throws Exception {
+        if(user == null)
+            throw new Exception(ErrorMessage.OBJECT_USER_IS_NULL);
         if(userDAO.getUserByUsername(user.getUsername()) != null)
             throw new IllegalArgumentException(ErrorMessage.USERNAME_IS_EXIST);
-        if(!userDAO.register(user))
-            throw new RuntimeException(ErrorMessage.REGISTER_FAILED);
+        if(userDAO.getUserByEmail(user.getEmail()) != null)
+            throw new IllegalArgumentException(ErrorMessage.EMAIL_IS_EXIST);
+        boolean success = userDAO.register(user);
+        if (!success) {
+            throw new Exception(ErrorMessage.REGISTER_FAILED);
+        }
         return true;
     }
 }
