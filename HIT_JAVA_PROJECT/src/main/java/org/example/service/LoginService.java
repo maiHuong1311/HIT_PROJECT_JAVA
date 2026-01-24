ac58ce494ee54c1a30586959149ea6c43866a055
maiHuong1311 <maihunw@gmail.com>
2026-01-24 23:59:25 +0700

    refactor(service): change login method return type to void

--- HIT_JAVA_PROJECT/src/main/java/org/example/service/LoginService.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/service/LoginService.java
@@ -3,18 +3,13 @@ package org.example.service;
 import org.example.constant.Common;
 import org.example.constant.ErrorMessage;
 import org.example.dao.UserDAO;
+import org.example.model.Role;
 import org.example.model.User;
 
 public class LoginService {
     private UserDAO userDAO = new UserDAO();
     public static User currentUser;
-    public boolean loginService(String username, String password, String role) {
-        if(username == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_USERNAME);
-        if(password == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_PASSWORD);
-        if(role == null)
-            throw new IllegalArgumentException(Common.NOT_NULL_ROLE);
+    public boolean loginService(String username, String password, Role role) {
         User user = userDAO.login(username, password, role);
         if(user == null)
             throw new IllegalArgumentException(ErrorMessage.LOGIN_FAILED);
