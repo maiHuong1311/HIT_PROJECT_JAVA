996b40a551c39151138fc902c41a6535cebd4918
maiHuong1311 <maihunw@gmail.com>
2026-01-25 23:56:36 +0700

    refactor: simplify validation and exception handling in RegisterController

--- HIT_JAVA_PROJECT/src/main/java/org/example/controller/RegisterController.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/controller/RegisterController.java
@@ -1,10 +1,17 @@
 package org.example.controller;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.example.constant.Common;
 import org.example.constant.ErrorMessage;
 import org.example.constant.SuccessfulMessage;
@@ -12,6 +19,7 @@ import org.example.model.User;
 import org.example.service.RegisterService;
 import org.example.utils.EmailUtil;
 import org.example.utils.PasswordUtil;
+import org.example.utils.SceneUtil;
 
 public class RegisterController {
     @FXML private TextField fullNameTextField;
@@ -28,55 +36,61 @@ public class RegisterController {
     @FXML private Label lblSuccessMessage;
     @FXML private Label lblErrorMessage;
     private RegisterService register = new RegisterService();
-    @FXML public void handleRegister() {
+    @FXML public void handleRegister(ActionEvent event) {
         clearAllErrorLabels();
-        try {
-            String fullName = fullNameTextField.getText().trim();
-            String username = usernameTextField.getText().trim();
-            String password = passwordField.getText().trim();String confirmPassword = confirmPasswordField.getText().trim();
-            String email = emailTextField.getText().trim();
-            boolean hasError = false;
-            if(fullName.isEmpty()) {
-                lblErrorFullName.setText(Common.NOT_NULL_FULL_NAME);
-                hasError = true;
-            }
-            if(username.isEmpty()) {
-                lblErrorUsername.setText(Common.NOT_NULL_USERNAME);
-                hasError = true;
-            }
-            if(password.isEmpty()) {
-                lblErrorPassword.setText(Common.NOT_NULL_PASSWORD);
-                hasError = true;
-            }
-            else if(!PasswordUtil.checkRegex(password)) {
-                lblErrorPassword.setText(ErrorMessage.INVALID_PASSWORD);
-                hasError = true;
-            }
-            if(!PasswordUtil.passwordConfirmation(password, confirmPassword)) {
-                lblErrorConfirmPassword.setText(ErrorMessage.INCORRECT_CONFIRM_PASSWORD);
-                hasError = true;
-            }
-            if(email.isEmpty()) {
-                lblErrorEmail.setText(Common.NOT_NULL_EMAIL);
-                hasError = true;
-            }
-            else if(!EmailUtil.checkRegex(email)) {
-                lblErrorEmail.setText(ErrorMessage.INVALID_EMAIL);
-                hasError = true;
-            }
-            if(hasError)
-                return;
-            boolean success = register.registerService(new User(fullName, username, password, email));
-            if(success)
-                lblSuccessMessage.setText(SuccessfulMessage.REGISTER_SUCCESSFULLY);
-            } catch(IllegalArgumentException e) {
-                if(e.getMessage().equals(ErrorMessage.EMAIL_IS_EXIST))
+        String fullName = fullNameTextField.getText().trim();
+        String username = usernameTextField.getText().trim();
+        String password = passwordField.getText().trim();
+        String confirmPassword = confirmPasswordField.getText().trim();
+        String email = emailTextField.getText().trim();
+        if(validateInput(fullName, username, password, confirmPassword, email)) {
+            try {
+                register.registerService(new User(fullName, username, password, email));
+                SceneUtil.showSuccessfulMessage(Common.SUCCESS_TITLE, Common.SUCCESS_HEADER, SuccessfulMessage.REGISTER_SUCCESSFULLY);
+                SceneUtil.changeScene(event, "/view/login.fxml", "Đăng nhập");
+            } catch (IllegalArgumentException e) {
+                if (e.getMessage().equals(ErrorMessage.EMAIL_IS_EXIST))
                     lblErrorEmail.setText(e.getMessage());
                 else
                     lblErrorUsername.setText(e.getMessage());
-            } catch(Exception e) {
-                lblErrorMessage.setText(ErrorMessage.REGISTER_FAILED);
+            } catch (Exception e) {
+                lblErrorMessage.setText(e.getMessage());
+                e.printStackTrace();
             }
+        }
+    }
+
+    private boolean validateInput(String fullName, String username, String password, String confirmPassword, String email) {
+        boolean hasError = false;
+        if(fullName.isEmpty()) {
+            lblErrorFullName.setText(Common.NOT_NULL_FULL_NAME);
+            hasError = true;
+        }
+        if(username.isEmpty()) {
+            lblErrorUsername.setText(Common.NOT_NULL_USERNAME);
+            hasError = true;
+        }
+        if(password.isEmpty()) {
+            lblErrorPassword.setText(Common.NOT_NULL_PASSWORD);
+            hasError = true;
+        }
+        else if(!PasswordUtil.checkRegex(password)) {
+            lblErrorPassword.setText(ErrorMessage.INVALID_PASSWORD);
+            hasError = true;
+        }
+        if(!PasswordUtil.passwordConfirmation(password, confirmPassword)) {
+            lblErrorConfirmPassword.setText(ErrorMessage.INCORRECT_CONFIRM_PASSWORD);
+            hasError = true;
+        }
+        if(email.isEmpty()) {
+            lblErrorEmail.setText(Common.NOT_NULL_EMAIL);
+            hasError = true;
+        }
+        else if(!EmailUtil.checkRegex(email)) {
+            lblErrorEmail.setText(ErrorMessage.INVALID_EMAIL);
+            hasError = true;
+        }
+        return !hasError;
     }
 
     private void clearAllErrorLabels() {
