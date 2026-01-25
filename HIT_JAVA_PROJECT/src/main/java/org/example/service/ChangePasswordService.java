6b06ce8b38ec9d0c4c6e1382c058aff5858ae75a
maiHuong1311 <maihunw@gmail.com>
2026-01-26 00:00:07 +0700

    refactor: optimize ChangePasswordService

--- HIT_JAVA_PROJECT/src/main/java/org/example/service/ChangePasswordService.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/service/ChangePasswordService.java
@@ -8,6 +8,7 @@ import org.example.model.OTP;
 import org.example.model.User;
 import org.example.utils.EmailUtil;
 import org.example.utils.OTPUtil;
+import org.example.utils.PasswordUtil;
 import java.time.LocalDateTime;
 
 public class ChangePasswordService {
@@ -20,32 +21,33 @@ public class ChangePasswordService {
         String otpCode = OTPUtil.generateOTP();
         OTP otp = new OTP(user.getId(), otpCode);
         otpDAO.saveOTP(otp);
-        boolean send = EmailUtil.sendEmail(user.getEmail(), Common.SUBJECT_EMAIL, Common.CONTENT_EMAIL + otp);
+        boolean send = EmailUtil.sendEmail(user.getEmail(), Common.SUBJECT_EMAIL, Common.CONTENT_EMAIL + otp.getCode());
         if(!send)
             throw new Exception(ErrorMessage.SEND_EMAIL_FAILED);
         return user.getId();
     }
 
-    public boolean verify(int userId, String inputOTP) throws Exception {
+    public void verify(int userId, String otpInput) throws Exception {
         OTP otp = otpDAO.getLatestOTP(userId);
         if(otp == null)
             throw new Exception(ErrorMessage.OTP_NOT_EXIST);
         if(otp.isUsed())
             throw new Exception(ErrorMessage.OTP_IS_USED);
-        if(!otp.getCode().equals(inputOTP))
+        if(!otp.getCode().equals(otpInput))
             throw new Exception(ErrorMessage.INVALID_OTP);
         if(LocalDateTime.now().isAfter(otp.getExpireAt()))
             throw new Exception(ErrorMessage.OTP_IS_EXPIRED);
         otpDAO.markOTPUsed(otp.getId());
-        return true;
     }
 
     public boolean resetPassword(int userId, String otpInput, String newPassword) throws Exception {
-        boolean isValid = verify(userId, otpInput);
-        if(isValid) {
-            userDAO.updatePassword(userId, newPassword);
-            return true;
+        this.verify(userId, otpInput);
+        if (newPassword == null) {
+            throw new Exception(Common.NOT_NULL_PASSWORD);
         }
-        return false;
+        if (!PasswordUtil.checkRegex(newPassword)) {
+            throw new Exception(ErrorMessage.INVALID_PASSWORD);
+        }
+        return userDAO.updatePassword(userId, newPassword);
     }
 }
