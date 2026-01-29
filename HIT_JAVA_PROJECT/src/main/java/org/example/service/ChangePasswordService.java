7ad5b2c91347374f440f3df4f89115a587f41939
maiHuong1311 <maihunw@gmail.com>
2026-01-29 23:26:33 +0700

    refactor: simplify logic in ChangePasswordService

--- HIT_JAVA_PROJECT/src/main/java/org/example/service/ChangePasswordService.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/service/ChangePasswordService.java
@@ -4,6 +4,10 @@ import org.example.constant.Common;
 import org.example.constant.ErrorMessage;
 import org.example.dao.OtpDAO;
 import org.example.dao.UserDAO;
+import org.example.exception.EmailServiceException;
+import org.example.exception.EntityNotFoundException;
+import org.example.exception.InvalidCredentialsException;
+import org.example.exception.InvalidOtpException;
 import org.example.model.OTP;
 import org.example.model.User;
 import org.example.utils.EmailUtil;
@@ -14,37 +18,33 @@ import java.time.LocalDateTime;
 public class ChangePasswordService {
     private UserDAO userDAO = new UserDAO();
     private OtpDAO otpDAO = new OtpDAO();
-    public int sendToEmail(String username) throws Exception {
+    public int sendToEmail(String username) {
         User user = userDAO.getUserByUsername(username);
         if(user == null)
-            throw new Exception(ErrorMessage.USER_NOT_EXIST);
+            throw new EntityNotFoundException(ErrorMessage.USER_NOT_EXIST);
         String otpCode = OTPUtil.generateOTP();
         OTP otp = new OTP(user.getId(), otpCode);
         otpDAO.saveOTP(otp);
         boolean send = EmailUtil.sendEmail(user.getEmail(), Common.SUBJECT_EMAIL, Common.CONTENT_EMAIL + otp.getCode());
         if(!send)
-            throw new Exception(ErrorMessage.SEND_EMAIL_FAILED);
+            throw new EmailServiceException(ErrorMessage.SEND_EMAIL_FAILED);
         return user.getId();
     }
 
-    public void verify(int userId, String otpInput) throws Exception {
+    public void verify(int userId, String otpInput) {
         OTP otp = otpDAO.getLatestOTP(userId);
         if(otp == null)
-            throw new Exception(ErrorMessage.OTP_NOT_EXIST);
+            throw new EntityNotFoundException(ErrorMessage.OTP_IS_NULL);
         if(otp.isUsed())
-            throw new Exception(ErrorMessage.OTP_IS_USED);
-        if(!otp.getCode().equals(otpInput))
-            throw new Exception(ErrorMessage.INVALID_OTP);
+            throw new InvalidOtpException(ErrorMessage.OTP_IS_USED);
         if(LocalDateTime.now().isAfter(otp.getExpireAt()))
-            throw new Exception(ErrorMessage.OTP_IS_EXPIRED);
+            throw new InvalidOtpException(ErrorMessage.OTP_IS_EXPIRED);
+        if(!otp.getCode().equals(otpInput))
+            throw new InvalidOtpException(ErrorMessage.INVALID_OTP);
         otpDAO.markOTPUsed(otp.getId());
     }
 
-    public boolean resetPassword(int userId, String otpInput, String newPassword) throws Exception {
-        this.verify(userId, otpInput);
-        if (!PasswordUtil.checkRegex(newPassword)) {
-            throw new Exception(ErrorMessage.INVALID_PASSWORD);
-        }
+    public boolean resetPassword(int userId, String newPassword) {
         return userDAO.updatePassword(userId, newPassword);
     }
 }
