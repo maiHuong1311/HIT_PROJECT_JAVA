525aae57ca6638cf91107c9c6aaf7067b164731e
maiHuong1311 <maihunw@gmail.com>
2026-01-29 23:23:45 +0700

    refactor(register): simplify logic in RegisterService

--- HIT_JAVA_PROJECT/src/main/java/org/example/service/RegisterService.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/service/RegisterService.java
@@ -9,16 +9,13 @@ import org.example.model.User;
 
 public class RegisterService {
     private UserDAO userDAO = new UserDAO();
-    public void registerService(User user) {
+    public boolean registerService(User user) {
         if(user == null)
             throw new EntityNotFoundException(ErrorMessage.SYSTEM_ERROR);
         if(userDAO.getUserByUsername(user.getUsername()) != null)
             throw new DuplicateEntityException(ErrorMessage.USERNAME_IS_EXIST);
         if(userDAO.getUserByEmail(user.getEmail()) != null)
             throw new DuplicateEntityException(ErrorMessage.EMAIL_IS_EXIST);
-        boolean success = userDAO.register(user);
-        if(!success) {
-            throw new AuthenticationException(ErrorMessage.REGISTER_FAILED);
-        }
+        return userDAO.register(user);
     }
 }
