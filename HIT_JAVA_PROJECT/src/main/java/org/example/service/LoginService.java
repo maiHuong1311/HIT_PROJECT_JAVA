9a68b015466225484ac9754d212ba707d81651bd
maiHuong1311 <maihunw@gmail.com>
2026-01-29 23:24:54 +0700

    refactor(login): rename exceptions for better clarity

--- HIT_JAVA_PROJECT/src/main/java/org/example/service/LoginService.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/service/LoginService.java
@@ -3,16 +3,17 @@ package org.example.service;
 import org.example.constant.Common;
 import org.example.constant.ErrorMessage;
 import org.example.dao.UserDAO;
+import org.example.exception.AuthenticationException;
 import org.example.model.Role;
 import org.example.model.User;
 
 public class LoginService {
     private UserDAO userDAO = new UserDAO();
     public static User currentUser;
-    public void loginService(String username, String password, Role role) throws Exception {
+    public void loginService(String username, String password, Role role) {
         User user = userDAO.login(username, password, role);
         if(user == null)
-            throw new Exception(ErrorMessage.LOGIN_FAILED);
+            throw new AuthenticationException(ErrorMessage.LOGIN_FAILED);
         currentUser = user;
     }
 }
