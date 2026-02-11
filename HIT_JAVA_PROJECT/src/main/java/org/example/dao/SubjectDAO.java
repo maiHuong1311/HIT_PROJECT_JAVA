4e4356768839ba46889f6f43c4cc92514df5a57b
maiHuong1311 <maihunw@gmail.com>
2026-02-11 22:25:56 +0700

    refactor: change searchSubject return type from Subject to List

--- HIT_JAVA_PROJECT/src/main/java/org/example/dao/SubjectDAO.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/dao/SubjectDAO.java
@@ -7,26 +7,30 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SubjectDAO {
-    public Subject searchSubject(String name) {
+    public List<Subject> searchSubject(String name) {
         String sql = "SELECT subjectId, name, totalQuestion, totalExercise FROM subject WHERE name = ? OR name like ?";
+        List<Subject> list = new ArrayList<>();
         try(Connection con = DBConnection.getConnection();
             PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, name);
             ps.setString(2, "%" + name + "%");
             ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
-                return new Subject(
+            while(rs.next()) {
+                Subject s = new Subject(
                         rs.getInt("subjectId"),
                         rs.getString("name"),
                         rs.getInt("totalQuestion"),
                         rs.getInt("totalExercise")
                 );
+                list.add(s);
             }
         } catch(SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return list;
     }
 }
