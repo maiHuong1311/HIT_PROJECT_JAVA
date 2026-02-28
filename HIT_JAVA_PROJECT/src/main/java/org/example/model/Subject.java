f68f1084868ed40e9597194e646577e940a550b8
maiHuong1311 <maihunw@gmail.com>
2026-02-28 08:41:11 +0700

    feat: update Subject model

--- HIT_JAVA_PROJECT/src/main/java/org/example/model/Subject.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/model/Subject.java
@@ -17,6 +17,9 @@ public class Subject {
     public int getSubjectId() {
         return subjectId;
     }
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
 
     public String getName() {
         return name;
