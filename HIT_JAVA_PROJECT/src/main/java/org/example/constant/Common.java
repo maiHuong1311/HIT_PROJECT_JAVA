c6612fd8916d8310f646beb6c3825f9cabc846ea
maiHuong1311 <maihunw@gmail.com>
2026-02-28 08:45:06 +0700

    feat: update Common constant

--- HIT_JAVA_PROJECT/src/main/java/org/example/constant/Common.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/constant/Common.java
@@ -14,4 +14,20 @@ public class Common {
     public static final String SUCCESS_HEADER = "Chúc mừng";
     public static final String NOT_NULL_OTP = "Mã OTP không được để trống";
     public static final String NOT_NULL_SUBJECT_NAME = "Nhập tên môn đi, năn nỉ á!";
+    public static final String SET_STYLE_HYPERLINK = "-fx-font-size: 15px; -fx-padding: 5 0 5 10; -fx-font-family: 'Arial'; -fx-text-fill:  #e6af55; -fx-font-weight: bold;";
+    public static final String COMPLETE_QUESTION = "Đã hoàn thành";
+    public static final String NOT_COMPLETE_QUESTION = "Chưa hoàn thành";
+    public static final String NOT_HAVE_MCQ = "Không có câu hỏi trắc nghiệm";
+    public static final String NOT_HAVE_EXERCISE = "Không có câu tự luận";
+    public static final String SET_STYLE_CORRECT_ANSWER = "-fx-background-color: #4CAF50; -fx-text-fill: white; -fx-background-radius: 5;";
+    public static final String SET_STYLE_INCORRECT_ANSWER = "-fx-background-color: #F44336; -fx-text-fill: white; -fx-background-radius: 5;";
+    public static final String CONFIRM_SET_TITLE = "Xác nhận tiến độ";
+    public static final String CONFIRM_SET_HEADER = "Bạn đã từng ôn bài này. ";
+    public static final String CONFIRM_SET_CONTENT = "Bạn muốn làm từ đầu hay tiếp tục";
+    public static final String CONTINUE_BUTTON = "Làm tiếp";
+    public static final String RESET_BUTTON = "Làm lại";
+    public static final String CANCEL_BUTTON = "Hủy";
+    public static final String USER_ANSWER = "Người dùng đã xem đáp án";
+    public static final String NULL_HISTORY = "Bạn chưa ôn môn nào";
+    public static final String NULL_QUESTION = "Bạn chưa ôn bải này";
 }
