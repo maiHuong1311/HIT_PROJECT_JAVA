f444f43165c90756668d3ee971afcea1b3ed47b4
maiHuong1311 <maihunw@gmail.com>
2026-02-28 08:38:48 +0700

    feat: update Lesson model

--- HIT_JAVA_PROJECT/src/main/java/org/example/model/Lesson.java
+++ HIT_JAVA_PROJECT/src/main/java/org/example/model/Lesson.java
@@ -4,12 +4,16 @@ public class Lesson {
     private int lessonId;
     private int subjectId;
     private String title;
+    private int totalQuestion;
+    private int totalExercise;
 
     public Lesson() {}
-    public Lesson(int lessonId, int subjectId, String title) {
+    public Lesson(int lessonId, int subjectId, String title, int totalQuestion, int totalExercise) {
         this.lessonId = lessonId;
         this.subjectId = subjectId;
         this.title = title;
+        this.totalQuestion = totalQuestion;
+        this.totalExercise = totalExercise;
     }
 
     public int getLessonId() {
@@ -28,5 +32,19 @@ public class Lesson {
     }
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public int getTotalQuestion() {
+        return totalQuestion;
+    }
+    public void setTotalQuestion(int totalQuestion) {
+        this.totalQuestion = totalQuestion;
+    }
+
+    public int getTotalExercise() {
+        return totalExercise;
+    }
+    public void setTotalExercise(int totalExercise) {
+        this.totalExercise = totalExercise;
     }
 }
